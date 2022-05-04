cebbe94aa114549f47d43f94d9280e832972ccee
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-04 23:28:46 +0300

    chore: update package-lock.json

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	presets: [['@babel/preset-env', { modules: false }]],
 	env: {
 		test: {
-			presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
+			presets: [['env', { targets: { node: 'current' } }]]
 		}
 	}
 }
