ea36a92c5a35e67beb5917ff3f3470e9692a59c4
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-04 19:09:55 +0300

    refactor: Change folder name from views to pages
    Changelog:
     - Changed pages folder name from views to pages
     - Rename HomeView and AboutView to HomePage and AboutPage
     - Update imports in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const HomePage = () =>
+	import(/* webpackChunkName: "about" */ '@/pages/HomePage.vue')
+const AboutPage = () =>
+	import(/* webpackChunkName: "about" */ '@/pages/AboutPage.vue')
 
 Vue.use(VueRouter)
 
@@ -8,16 +12,12 @@ const routes = [
 	{
 		path: '/',
 		name: 'home',
-		component: HomeView
+		component: HomePage
 	},
 	{
 		path: '/about',
 		name: 'about',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () =>
-			import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+		component: AboutPage
 	}
 ]
 
