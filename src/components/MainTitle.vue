37ca61bf06a7c906ab2b74b993fe3cfd858b7c1f
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-05 18:14:51 +0300

    feat: Created MainTitle component
    Changelog:
     - Created MainTitle component with prop visibility(If visibility =
       false, component be in DOM, but now shows on browsers, its necessary for SEO )
     - Deleted HelloWorld component

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1 :class="['title', { 'visually-hidden': !visibility }]">
+		Agridera Seeds and Agriculture Ltd
+	</h1>
+</template>
+
+<script>
+export default {
+	props: {
+		visibility: {
+			type: Boolean,
+			required: false,
+			default: true
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped src="@/style/MainTitle.scss"/>
