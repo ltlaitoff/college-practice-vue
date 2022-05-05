06756fc0cb3076a6c907535b984fe2dc9dd8e40e
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-05 14:31:07 +0300

    test: Created tests for AGHeader

---
+++ src/tests/components/AGHeader.spec.js
@@ -0,0 +1,61 @@
+import { shallowMount } from '@vue/test-utils'
+import AGHeader from '@/components/AGHeader'
+import MainNavigation from '@/components/MainNavigation'
+
+import {
+	BButton,
+	BContainer,
+	BFormInput,
+	BIcon,
+	BImg,
+	BLink
+} from 'bootstrap-vue'
+
+import 'jsdom-global'
+
+describe('AGHeader', () => {
+	let wrapper = null
+
+	beforeEach(() => {
+		wrapper = shallowMount(AGHeader, {
+			stubs: {
+				'b-container': BContainer,
+				'b-link': BLink,
+				'b-img': BImg,
+				'b-form-input': BFormInput,
+				'b-button': BButton,
+				'b-icon': BIcon
+			}
+		})
+	})
+
+	afterEach(() => {
+		wrapper.destroy()
+	})
+
+	it('BContainer should be have children header component', () => {
+		expect(
+			wrapper.getComponent('.container').getComponent('header').exists()
+		).toBe(true)
+	})
+
+	it('Search input should be in component', () => {
+		expect(wrapper.findComponent('input.search').exists()).toBe(true)
+	})
+
+	it('Button should be in header and should be have a icon as children', () => {
+		const header = wrapper.findComponent('header')
+		const button = header.findComponent('button.button')
+		const icon = button.findComponent('.b-icon')
+
+		expect(button.exists()).toBe(true)
+		expect(icon.exists()).toBe(true)
+	})
+
+	it('MainNavigation should be in header', () => {
+		const header = wrapper.findComponent('header')
+		const mainNavigation = header.findComponent(MainNavigation)
+
+		expect(mainNavigation.exists()).toBe(true)
+	})
+})
