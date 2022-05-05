3a481305f99fe49bebc2954d579e7e01c851cbc9
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-05 20:10:54 +0300

    Created HomeAbout component

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -2,18 +2,21 @@
 	<div class="home">
 		<HomePromo />
 		<SubProducts />
+		<HomeAbout />
 	</div>
 </template>
 
 <script>
 import HomePromo from './components/HomePromo.vue'
 import SubProducts from './components/SubProducts.vue'
+import HomeAbout from './components/HomeAbout.vue'
 
 export default {
 	name: 'HomePage',
 	components: {
 		HomePromo,
-		SubProducts
+		SubProducts,
+		HomeAbout
 	}
 }
 </script>
