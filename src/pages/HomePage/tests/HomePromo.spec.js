99c0432b06554d4d21565a60ff93c713d7776153
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-05 18:58:28 +0300

    tests: Created tests for HomePromo

---
+++ src/pages/HomePage/tests/HomePromo.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils'
+import HomePromo from '../components/HomePromo'
+import MainTitle from '@/components/MainTitle'
+import 'jsdom-global'
+
+import { BButton } from 'bootstrap-vue'
+
+describe('AGFooter', () => {
+	let wrapper = null
+
+	beforeEach(() => {
+		wrapper = shallowMount(HomePromo, {
+			stubs: {
+				'b-button': BButton
+			}
+		})
+	})
+
+	afterEach(() => {
+		wrapper.destroy()
+	})
+
+	it('HomePromo should have MainTitle', () => {
+		expect(wrapper.findComponent(MainTitle).exists()).toBe(true)
+	})
+
+	it('HomePromo should have b-button with text read more', () => {
+		expect(wrapper.findComponent(BButton).exists()).toBe(true)
+		expect(wrapper.findComponent(BButton).text()).toBe('read more')
+	})
+})
