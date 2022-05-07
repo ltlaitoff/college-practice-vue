dc7ead457ecc682b8ed628ebe5d0f82a2fa09ea4
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-07 15:34:43 +0300

    feat: Created second header style for all pages, besides home

--- src/components/AGHeader.vue
+++ src/components/AGHeader.vue
@@ -1,21 +1,26 @@
 <template>
-	<b-container>
-		<header class="header">
-			<b-link to="/"
-				><b-img src="@/assets/images/logo.png" fluid class="logo"
-			/></b-link>
-			<div class="wrapper">
-				<div class="search-wrapper">
-					<b-form-input type="search" class="search" placeholder="Search" />
-					<b-button class="button" variant="light">
-						<b-icon icon="search" />
-					</b-button>
-				</div>
+	<div class="full">
+		<b-container class="hcontainer">
+			<header class="header">
+				<b-link to="/"
+					><b-img
+						src="@/assets/images/logo.png"
+						fluid
+						:class="checkOnHomePage ? 'logo-promo' : 'logo'"
+				/></b-link>
+				<div :class="['wrapper', { 'wrapper-promo': checkOnHomePage }]">
+					<div class="search-wrapper">
+						<b-form-input type="search" class="search" placeholder="Search" />
+						<b-button class="button" variant="light">
+							<b-icon icon="search" />
+						</b-button>
+					</div>
 
-				<MainNavigation class="navigation" />
-			</div>
-		</header>
-	</b-container>
+					<MainNavigation class="navigation" />
+				</div>
+			</header>
+		</b-container>
+	</div>
 </template>
 
 <script>
@@ -24,7 +29,12 @@ import { BIcon } from 'bootstrap-vue'
 
 export default {
 	name: 'AGHeader',
-	components: { MainNavigation, BIcon }
+	components: { MainNavigation, BIcon },
+	computed: {
+		checkOnHomePage() {
+			return this.$route.path === '/'
+		}
+	}
 }
 </script>
 
