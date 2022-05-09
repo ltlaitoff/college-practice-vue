19bda0323584678213cd32bd2150ae069f56a69a
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-09 19:58:33 +0300

    feat: added product info in apiData
    Changelog:
     - Added attentionIcon in category and imported icons
     - Added productBG in category and imported bgs
     - Added attentionText in product
     - Added info in product

--- src/api/apiData.js
+++ src/api/apiData.js
@@ -1,51 +1,323 @@
+/*
+	Добавить в категорию "крассную иконку"
+	Добавить в категорию задний фон товара
+	Добавить в товар "крассный текст"
+	Добавить информацию о товаре
+*/
+
 const tomatoProducts = [
 	{
 		id: 0,
 		name: 'TAG 1000 (TAG 853)',
 		type: 'Tomato Determinate Red Round',
-		image: 'tomato-1000'
+		image: 'tomato-1000',
+		attentionText: 'Very high yield and excellent fruits quality!',
+		info: [
+			{
+				id: 0,
+				title: 'CHARACTERISTICS',
+				text: 'Resistance\nHR: ToMV:0-2; Fol: 1,2; Ve/Vd;\nIR: TYLCV'
+			},
+			{
+				id: 1,
+				title: 'Plant',
+				texts: [
+					'Strong vigor that provides good leaf coverage.',
+					'Very high productivity with good fruit setting.',
+					'Early matured variety.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Fruit',
+				texts: [
+					'Long shelf life on plant and post harvest.',
+					'Nice shiny attractive deep red color.',
+					'Average fruit size: 140 – 160 grams.'
+				]
+			},
+			{
+				id: 3,
+				title: 'Cycle',
+				texts: ['Spring', 'Fall', 'Winter']
+			},
+			{
+				id: 4,
+				title: 'Color',
+				text: 'Red'
+			}
+		]
 	},
 	{
 		id: 1,
 		name: 'TAG 1001 (TAG 855)',
 		type: 'Tomato Determinate Red Standard',
-		image: 'tomato-1001'
+		image: 'tomato-1001',
+		attentionText: '',
+		info: [
+			{
+				id: 0,
+				title: '',
+				texts: ['', '']
+			},
+			{
+				id: 1,
+				title: '',
+				texts: ['', '']
+			}
+		]
 	},
 	{
 		id: 2,
 		name: 'TAG 1002 (TAG 809)',
 		type: 'Tomato Determinate Red Standard-round',
-		image: 'tomato-1002'
+		image: 'tomato-1002',
+		attentionText: 'Very high yield and excellent fruits quality!',
+		info: [
+			{
+				id: 0,
+				title: 'CHARACTERISTICS',
+				text: 'Resistance\nHR: ToMV:0-2; Fol: 1,2; Ve/Vd;\nIR: TYLCV'
+			},
+			{
+				id: 1,
+				title: 'Plant',
+				texts: [
+					'Strong vigor that provides good leaf coverage.',
+					'Very high productivity with good fruit setting.',
+					'Early matured variety.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Fruit',
+				texts: [
+					'Long shelf life on plant and post harvest.',
+					'Nice shiny attractive deep red color.',
+					'Average fruit size: 140 – 160 grams.'
+				]
+			},
+			{
+				id: 3,
+				title: 'Cycle',
+				texts: ['Spring', 'Fall', 'Winter']
+			},
+			{
+				id: 4,
+				title: 'Color',
+				text: 'Red'
+			}
+		]
 	},
 	{
 		id: 3,
 		name: 'TAG 1003 (TAG 834)',
 		type: 'Tomato Determinate Red',
-		image: 'tomato-1003'
+		image: 'tomato-1003',
+		attentionText: 'Very high yield and excellent fruits quality!',
+		info: [
+			{
+				id: 0,
+				title: 'CHARACTERISTICS',
+				text: 'Resistance\nHR: ToMV:0-2; Fol: 1,2; Ve/Vd;\nIR: TYLCV'
+			},
+			{
+				id: 1,
+				title: 'Plant',
+				texts: [
+					'Strong vigor that provides good leaf coverage.',
+					'Very high productivity with good fruit setting.',
+					'Early matured variety.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Fruit',
+				texts: [
+					'Long shelf life on plant and post harvest.',
+					'Nice shiny attractive deep red color.',
+					'Average fruit size: 140 – 160 grams.'
+				]
+			},
+			{
+				id: 3,
+				title: 'Cycle',
+				texts: ['Spring', 'Fall', 'Winter']
+			},
+			{
+				id: 4,
+				title: 'Color',
+				text: 'Red'
+			}
+		]
 	},
 	{
 		id: 4,
 		name: 'TAG 1004 (TAG 848)',
 		type: 'Tomato Determinate Red BEEF Round',
-		image: 'tomato-1004'
+		image: 'tomato-1004',
+		attentionText: 'Very high yield and excellent fruits quality!',
+		info: [
+			{
+				id: 0,
+				title: 'CHARACTERISTICS',
+				text: 'Resistance\nHR: ToMV:0-2; Fol: 1,2; Ve/Vd;\nIR: TYLCV'
+			},
+			{
+				id: 1,
+				title: 'Plant',
+				texts: [
+					'Strong vigor that provides good leaf coverage.',
+					'Very high productivity with good fruit setting.',
+					'Early matured variety.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Fruit',
+				texts: [
+					'Long shelf life on plant and post harvest.',
+					'Nice shiny attractive deep red color.',
+					'Average fruit size: 140 – 160 grams.'
+				]
+			},
+			{
+				id: 3,
+				title: 'Cycle',
+				texts: ['Spring', 'Fall', 'Winter']
+			},
+			{
+				id: 4,
+				title: 'Color',
+				text: 'Red'
+			}
+		]
 	},
 	{
 		id: 5,
 		name: 'TAG 1005 (TAG 800) ',
 		type: 'Tomato Determinate Red Standard-round-2',
-		image: 'tomato-1005'
+		image: 'tomato-1005',
+		attentionText: 'Very high yield and excellent fruits quality!',
+		info: [
+			{
+				id: 0,
+				title: 'CHARACTERISTICS',
+				text: 'Resistance\nHR: ToMV:0-2; Fol: 1,2; Ve/Vd;\nIR: TYLCV'
+			},
+			{
+				id: 1,
+				title: 'Plant',
+				texts: [
+					'Strong vigor that provides good leaf coverage.',
+					'Very high productivity with good fruit setting.',
+					'Early matured variety.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Fruit',
+				texts: [
+					'Long shelf life on plant and post harvest.',
+					'Nice shiny attractive deep red color.',
+					'Average fruit size: 140 – 160 grams.'
+				]
+			},
+			{
+				id: 3,
+				title: 'Cycle',
+				texts: ['Spring', 'Fall', 'Winter']
+			},
+			{
+				id: 4,
+				title: 'Color',
+				text: 'Red'
+			}
+		]
 	},
 	{
 		id: 6,
 		name: 'TAG 1006 (TAG 898)',
 		type: 'Tomato Determinate Red Elongated',
-		image: 'tomato-1006'
+		image: 'tomato-1006',
+		attentionText: 'Very high yield and excellent fruits quality!',
+		info: [
+			{
+				id: 0,
+				title: 'CHARACTERISTICS',
+				text: 'Resistance\nHR: ToMV:0-2; Fol: 1,2; Ve/Vd;\nIR: TYLCV'
+			},
+			{
+				id: 1,
+				title: 'Plant',
+				texts: [
+					'Strong vigor that provides good leaf coverage.',
+					'Very high productivity with good fruit setting.',
+					'Early matured variety.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Fruit',
+				texts: [
+					'Long shelf life on plant and post harvest.',
+					'Nice shiny attractive deep red color.',
+					'Average fruit size: 140 – 160 grams.'
+				]
+			},
+			{
+				id: 3,
+				title: 'Cycle',
+				texts: ['Spring', 'Fall', 'Winter']
+			},
+			{
+				id: 4,
+				title: 'Color',
+				text: 'Red'
+			}
+		]
 	},
 	{
 		id: 7,
 		name: 'TAG 1007 (TAG 816)',
 		type: 'Tomato Determinate Red Alon',
-		image: 'tomato-1007'
+		image: 'tomato-1007',
+		attentionText: 'Very high yield and excellent fruits quality!',
+		info: [
+			{
+				id: 0,
+				title: 'CHARACTERISTICS',
+				text: 'Resistance\nHR: ToMV:0-2; Fol: 1,2; Ve/Vd;\nIR: TYLCV'
+			},
+			{
+				id: 1,
+				title: 'Plant',
+				texts: [
+					'Strong vigor that provides good leaf coverage.',
+					'Very high productivity with good fruit setting.',
+					'Early matured variety.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Fruit',
+				texts: [
+					'Long shelf life on plant and post harvest.',
+					'Nice shiny attractive deep red color.',
+					'Average fruit size: 140 – 160 grams.'
+				]
+			},
+			{
+				id: 3,
+				title: 'Cycle',
+				texts: ['Spring', 'Fall', 'Winter']
+			},
+			{
+				id: 4,
+				title: 'Color',
+				text: 'Red'
+			}
+		]
 	}
 ]
 
@@ -54,61 +326,341 @@ const ornamentalSunflowersProducts = [
 		id: 0,
 		name: 'Tabor Double (859)',
 		type: 'Classic',
-		image: 'ornamental-859'
+		image: 'ornamental-859',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 1,
 		name: 'Tabor Lemon - 1235',
 		type: 'Lemon classic',
-		image: 'ornamental-1235'
+		image: 'ornamental-1235',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 2,
 		name: 'Tabor Orange 1252',
 		type: 'Yellow core',
-		image: 'ornamental-1252'
+		image: 'ornamental-1252',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 3,
 		name: 'Maya Orange - JG-98',
 		type: 'Orange classic',
-		image: 'ornamental-98'
+		image: 'ornamental-98',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 4,
 		name: 'Omri Supreme - JG-214',
 		type: 'Full flower',
-		image: 'ornamental-214'
+		image: 'ornamental-214',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 5,
 		name: 'Maya Gold Supreme - DC 135',
 		type: 'Yellow core',
-		image: 'ornamental-135'
+		image: 'ornamental-135',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 6,
 		name: 'Tanja - 500',
 		type: 'BI Color',
-		image: 'ornamental-500'
+		image: 'ornamental-500',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 7,
 		name: 'Helianthus Flame 893 ',
 		type: 'Lemon classic',
-		image: 'ornamental-893'
+		image: 'ornamental-893',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 8,
 		name: 'Carmel 337',
 		type: 'BI Color',
-		image: 'ornamental-337'
+		image: 'ornamental-337',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	},
 	{
 		id: 9,
 		name: 'Galilee Orange 460',
 		type: 'Lemon classic',
-		image: 'ornamental-460'
+		image: 'ornamental-460',
+		attentionText: 'Grows year-round.',
+		info: [
+			{
+				id: 0,
+				title: 'Plant',
+				texts: [
+					'Height of plant – 130 to 170 cm.',
+					'Plant not branching.',
+					'Smooth leaves.',
+					'High vigor.'
+				]
+			},
+			{
+				id: 2,
+				title: 'Flower',
+				texts: ['Orange petals.', 'Black core.', 'High uniformity.']
+			},
+			{
+				id: 3,
+				title: 'Shelf life',
+				texts: ['Significantly long.', 'Blooming: 55 days in spring.']
+			},
+			{
+				id: 4,
+				title: 'Resistance',
+				text: 'Tolerant to powdery and downy mildews'
+			}
+		]
 	}
 ]
 
@@ -117,49 +669,153 @@ const wheatProducts = [
 		id: 0,
 		name: 'Yuval',
 		type: 'Spring Wheat',
-		image: 'wheat-yuval'
+		image: 'wheat-yuval',
+		info: [
+			{
+				id: 0,
+				title: 'Summary',
+				texts: [
+					'Very Late variety.',
+					'Tall, resistance to lodging.',
+					'Suitable to good rainfall conditions. ',
+					'Very high biomass and exceptional high yield.',
+					'Resistance to Local Leaf rust and Stem rust.'
+				]
+			}
+		]
 	},
 	{
 		id: 1,
 		name: 'Nirit',
 		type: 'Spring Wheat',
-		image: 'wheat-nirit'
+		image: 'wheat-nirit',
+		info: [
+			{
+				id: 0,
+				title: 'Summary',
+				texts: [
+					'Very Late variety.',
+					'Tall, resistance to lodging.',
+					'Suitable to good rainfall conditions. ',
+					'Very high biomass and exceptional high yield.',
+					'Resistance to Local Leaf rust and Stem rust.'
+				]
+			}
+		]
 	},
 	{
 		id: 2,
 		name: 'Gedera',
 		type: 'Spring Wheat',
-		image: 'wheat-gedera'
+		image: 'wheat-gedera',
+		info: [
+			{
+				id: 0,
+				title: 'Summary',
+				texts: [
+					'Very Late variety.',
+					'Tall, resistance to lodging.',
+					'Suitable to good rainfall conditions. ',
+					'Very high biomass and exceptional high yield.',
+					'Resistance to Local Leaf rust and Stem rust.'
+				]
+			}
+		]
 	},
 	{
 		id: 3,
 		name: 'Omer',
 		type: 'Spring Wheat',
-		image: 'wheat-omer'
+		image: 'wheat-omer',
+		info: [
+			{
+				id: 0,
+				title: 'Summary',
+				texts: [
+					'Very Late variety.',
+					'Tall, resistance to lodging.',
+					'Suitable to good rainfall conditions. ',
+					'Very high biomass and exceptional high yield.',
+					'Resistance to Local Leaf rust and Stem rust.'
+				]
+			}
+		]
 	},
 	{
 		id: 4,
 		name: 'C-9 ',
 		type: 'DURUM',
-		image: 'wheat-c-9'
+		image: 'wheat-c-9',
+		info: [
+			{
+				id: 0,
+				title: 'Summary',
+				texts: [
+					'Very Late variety.',
+					'Tall, resistance to lodging.',
+					'Suitable to good rainfall conditions. ',
+					'Very high biomass and exceptional high yield.',
+					'Resistance to Local Leaf rust and Stem rust.'
+				]
+			}
+		]
 	},
 	{
 		id: 5,
 		name: 'Amit',
 		type: 'Spring Wheat',
-		image: 'wheat-amit'
+		image: 'wheat-amit',
+		info: [
+			{
+				id: 0,
+				title: 'Summary',
+				texts: [
+					'Very Late variety.',
+					'Tall, resistance to lodging.',
+					'Suitable to good rainfall conditions. ',
+					'Very high biomass and exceptional high yield.',
+					'Resistance to Local Leaf rust and Stem rust.'
+				]
+			}
+		]
 	},
 	{
 		id: 6,
 		name: 'DAR',
 		type: 'Spring Wheat (silage)',
-		image: 'wheat-dar'
+		image: 'wheat-dar',
+		info: [
+			{
+				id: 0,
+				title: 'Summary',
+				texts: [
+					'Very Late variety.',
+					'Tall, resistance to lodging.',
+					'Suitable to good rainfall conditions. ',
+					'Very high biomass and exceptional high yield.',
+					'Resistance to Local Leaf rust and Stem rust.'
+				]
+			}
+		]
 	},
 	{
 		id: 7,
 		name: 'AG-78 (new)',
 		type: 'Spring Type grains & silage',
-		image: 'wheat-ag-78-new'
+		image: 'wheat-ag-78-new',
+		info: [
+			{
+				id: 0,
+				title: 'Summary',
+				texts: [
+					'Very Late variety.',
+					'Tall, resistance to lodging.',
+					'Suitable to good rainfall conditions. ',
+					'Very high biomass and exceptional high yield.',
+					'Resistance to Local Leaf rust and Stem rust.'
+				]
+			}
+		]
 	}
 ]
 
@@ -171,7 +827,9 @@ export const products = [
 		categoryMinifyName: 'tomato',
 		categoryImage: 'tomato',
 		disabled: false,
-		products: tomatoProducts
+		products: tomatoProducts,
+		attentionIcon: 'tomato-attention-icon',
+		productBG: 'tomato-product-bg.svg'
 	},
 	{
 		id: 1,
@@ -180,7 +838,9 @@ export const products = [
 		categoryMinifyName: 'ornamentalSunflowers',
 		categoryImage: 'ornamentalSunflowers',
 		disabled: false,
-		products: ornamentalSunflowersProducts
+		products: ornamentalSunflowersProducts,
+		attentionIcon: 'ornamentalSunflowers-attention-icon.svg',
+		productBG: 'ornamentalSunflowers-product-bg.svg'
 	},
 	{
 		id: 2,
@@ -189,7 +849,8 @@ export const products = [
 		categoryMinifyName: 'wheat',
 		categoryImage: 'wheat',
 		disabled: false,
-		products: wheatProducts
+		products: wheatProducts,
+		productBG: 'wheat-product-bg.svg'
 	},
 	{
 		id: 3,
