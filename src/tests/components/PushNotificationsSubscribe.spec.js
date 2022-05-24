4373a8455e0e65ad72de32ddcbce78ee1c5a6174
Ivan Baluta <ltlaitoff@gmail.com>
2022-05-24 15:03:04 +0300

    feat: Created push notifications

---
+++ src/tests/components/PushNotificationsSubscribe.spec.js
@@ -0,0 +1,11 @@
+// import { shallowMount } from '@vue/test-utils'
+// import PushNotificationsSubscribe from '@/components/PushNotificationsSubscribe'
+import 'jsdom-global'
+
+import TestsConsoleLog from '@/testHelpers/colors'
+describe('PushNotificationsSubscribe', () => {
+	TestsConsoleLog.fixme('FIXME: Create tests for PushNotificationsSubscribe')
+	it('-', () => {
+		expect(true).toBe(true)
+	})
+})
